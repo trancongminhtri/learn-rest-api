b0f3829a2cc7e99ca967dfbcfe1bf0160f7b4d55
MinhTri/trancongminhtri@gmail.com <trancongminhtri@gmail.com>
2021-11-30 11:21:36 +0700

    login, logout, register

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
-
-use Illuminate\Http\Request;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,7 +13,37 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+// ======== Public routes =======
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('/product', [ProductController::class, 'index']);
+
+Route::get('/product/{id}', [ProductController::class, 'show']);
+
+Route::get('/product/search/{name}', [ProductController::class, 'search']);
+
+// Route::resource('product', ProductController::class);
+
+
+// ======== Protected routes =======
+
+// Route::middleware('auth:sanctum')->get('/user', function () {
+//     Route::get('/product/search/{name}', [ProductController::class, 'search']);
+// });
+
+// chuyển đổi qua dòng này
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/product', [ProductController::class, 'store']);
+    Route::put('/product/{id}', [ProductController::class, 'update']);
+    Route::delete('/product/{id}', [ProductController::class, 'destroy']);
+    // Route::get('/product/search/{name}', [ProductController::class, 'search']);
+
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
